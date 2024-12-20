ae49a4bc84860cea9e28bea0f0ef3295f12ecc38
polikmm <polikmm@mail.ru>
2024-12-20 18:15:33 +0400

    feat: add solution to src/dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const names = members.filter((name) => typeof name === 'string')
+    .map((name) => name.trim())
+    .filter((name) => name.match(/\w+/))
+    .reduce((team, name) => team += name[0].toLowerCase(), '')
+    .split('')
+    .sort()
+    .join('')
+    .toUpperCase();
+  return names;
 }
 
 module.exports = {
