46b4f9a1c33c85d6c732f5a1c674d4711624bcaf
polikmm <polikmm@mail.ru>
2024-12-20 20:23:55 +0400

    feat: add solution to src/extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr = String(str);
+  const times = options.repeatTimes;
+  const sep = options.separator || '+';
+  const add = options.hasOwnProperty('addition') ? String(options.addition) : options.addition;
+  const addTimes = options.additionRepeatTimes;
+  const addSep = options.additionSeparator || '|';
+
+  if (!add && !addTimes) {
+    for (let i = 1; i < times; i += 1) {
+      newStr = `${newStr}${sep}${str}`;
+    }
+    return newStr;
+  }
+  if (!addSep && add) {
+    for (let i = 1; i < times; i += 1) {
+      const additional = add.repeat(addTimes);
+      newStr = `${newStr}${additional}${sep}${str}`;
+      if (i === times - 1) newStr += `${additional}`;
+    }
+    return newStr;
+  }
+  if (!times && !addTimes) {
+    newStr = `${newStr}${add}`;
+    return newStr;
+  }
+  if (times && add && addTimes && addSep) {
+    const addition = new Array(addTimes * 2 - 1)
+      .fill(add)
+      .map((elem, idx) => idx % 2 === 1 ? elem = addSep : elem)
+      .join('');
+      for (let i = 1; i <= times; i += 1) {
+        if (i === times) {
+          newStr = `${newStr}${addition}`;
+        } else {
+          newStr = `${newStr}${addition}${sep}${str}`;
+        }
+      }
+      return newStr;
+    }
+  return `${newStr.repeat(times)}${sep}${newStr.repeat(times)}`;
 }
 
 module.exports = {
